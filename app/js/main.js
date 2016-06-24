c5cd296a39c3af21d5eca0dcf666496228fec166
jeremygholland <jeremygholland@gmail.com>
2016-06-24 13:54:23 -0500

    changed conventions of data recording to allow for object notation

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,27 @@
 var app = angular.module('app', [])
 
 app.controller('myCtrl', function ($scope) {
-  $scope.clickMe= function () {
+  $scope.clickMe= function() {
     var heroOne = $scope.heroOne;
-    alert(heroOne)  
+    $.ajax({
+      type:"GET",
+      url: 'http://gateway.marvel.com:80/v1/public/characters?name=' + heroOne +
+				'&limit=100&apikey=64f1f5a1ab896a13dd9c6b4009b0817e',
+      dataType: 'json',
+      success: function(json){
+        $scope.firstSearchResult ={
+            id:json.data.results[0].id,
+            description: json.data.results[0].description,
+            img: json.data.results[0].thumbnail.path + '/detail.jpg',
+            name: json.data.results[0].name
+        }
+
+      },
+      error: function() {
+        console.log('coulnd\'t that hero.')
+
+      }
+    })
 
   }
 })
