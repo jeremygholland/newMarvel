cc659553e9151e5b3c61e5b9adf65843c1204c43
jeremygholland <jeremygholland@gmail.com>
2016-06-24 15:11:47 -0500

    included clearing function

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,24 @@
 var app = angular.module('app', [])
 
 app.controller('myCtrl', function ($scope) {
+  function clear() {
+      $scope.heroOneReturn= {
+        id: '',
+        description: '',
+        img: '',
+        name: '',
+      }
+      $scope.heroTwoReturn= {
+        id: '',
+        description: '',
+        img: '',
+        name: ''
+      }
+  }
+
+  var apiKey = '64f1f5a1ab896a13dd9c6b4009b0817e';
   $scope.clickMe= function() {
+    clear();
     var heroOne = $scope.heroOne;
     $.ajax({
       type:"GET",
@@ -9,7 +26,7 @@ app.controller('myCtrl', function ($scope) {
 				'&limit=100&apikey=64f1f5a1ab896a13dd9c6b4009b0817e',
       dataType: 'json',
       success: function(json){
-        $scope.firstSearchResult ={
+        $scope.heroOneReturn ={
             id:json.data.results[0].id,
             description: json.data.results[0].description,
             img: json.data.results[0].thumbnail.path + '/detail.jpg',
@@ -23,5 +40,29 @@ app.controller('myCtrl', function ($scope) {
       }
     })
 
+    var heroTwo = $scope.heroTwo;
+    $.ajax({
+      type: "GET",
+      url: 'http://gateway.marvel.com:80/v1/public/characters?name=' + heroTwo +
+				'&limit=100&apikey='+apiKey,
+        dataType: 'json',
+        success:function(json){
+          $scope.heroTwoReturn= {
+            id:json.data.results[0].id,
+            description: json.data.results[0].description,
+            img: json.data.results[0].thumbnail.path + '/detail.jpg',
+            name: json.data.results[0].name
+          }
+        },
+        error: function(){
+          $scope.heroTwoReturn.name= "this name didn't work."
+        }
+    })
+
+    setTimeout(function(){
+   location = ''
+ },10000)
   }
+
+
 })
